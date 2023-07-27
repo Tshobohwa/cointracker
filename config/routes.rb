a4df3031d27b141f1a914da5080fec98ff9f4458
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-27 12:13:54 +0200

    Create group controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "group#index"
 
   # Defines the root path route ("/")
   # root "articles#index"
